6f62fba84d51dd129620ebc817339d4b4c3d48ae
Crazzy4999 <48382037+Crazzy4999@users.noreply.github.com>
2019-03-10 12:15:54 +0100

    Add files via upload
    This is the latest 2019.03.10.

---
+++ main/java/almos/BetterBiomes/BetterBiomes.java
@@ -0,0 +1,65 @@
+package almos.BetterBiomes;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import almos.BetterBiomes.lists.ItemList;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod("betterbiomes")
+public class BetterBiomes
+{
+	public static BetterBiomes instance;
+	public static final String modid = "betterbiomesmod";
+	public static Logger logger = LogManager.getLogger("modid");
+	
+	public BetterBiomes() 
+	{
+		instance = this;
+		
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientRegistries);
+		
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	private void setup(final FMLCommonSetupEvent event)
+	{
+		logger.info("Setup method registered.");
+	}
+	
+	private void clientRegistries(final FMLClientSetupEvent event)
+	{
+		logger.info("clientRegistries method registered.");
+	}
+	
+	@Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
+	public static class RegistryEvents
+	{
+		@SubscribeEvent
+		public static void registerItems(final RegistryEvent.Register<Item> event)
+		{
+			event.getRegistry().registerAll
+			(
+					ItemList.jade_gem = new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName(location("jade_gem"))
+			);
+			
+			logger.info("Items registered.");	
+		}
+		
+		private static ResourceLocation location(String name)
+		{
+			return new ResourceLocation(modid, name);
+		}
+		
+	}
+}
---
+++ main/java/almos/BetterBiomes/lists/ItemList.java
@@ -0,0 +1,8 @@
+package almos.BetterBiomes.lists;
+
+import net.minecraft.item.Item;
+
+public class ItemList
+{
+	public static Item jade_gem;
+}
